3effd256dc028f9532d0b29656d689bd80551d3b
tharukeRajapaksha123 <darksparrow156@gmail.com>
2022-01-03 21:06:29 +0530

    ADD: add post done

---
+++ www/js/add_post.js
@@ -0,0 +1,10 @@
+function addPost(e){
+    e.preventDefault()
+    const title = document.querySelector(".title")
+    const content = document.querySelector(".content")
+    const meata_title = document.querySelector(".meta-title")
+    const meata_description = document.querySelector(".meta-description")
+    const download_link = document.querySelector(".download-link")
+
+    console.log(title)
+}
--- www/js/index.js
+++ www/js/index.js
@@ -37,3 +37,4 @@ async function fetchFirmwareData (){
 }
 
 fetchFirmwareData()
+
